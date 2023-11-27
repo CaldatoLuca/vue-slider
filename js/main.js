736b6ad8405117056056ccc4bb1a8a120baccf41
CaldatoLuca <lucacaldato15@gmail.com>
2023-11-27 16:28:05 +0100

    feat: implementazione dei data

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      mainTitle: "Carosello news",
+      images: {
+        title: [
+          "Uomo sul Lago",
+          "Paesino sul Lago",
+          "Londra",
+          "Città",
+          "Caraibi",
+        ],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        src: "img/01.jpg",
+      },
+    };
   },
 }).mount("#app");
